2dff3ac185212ab0540929739a0ffaea2b4dfade
dhwang <chadole815@naver.com>
2021-10-11 15:11:22 +0900

    rush00version2

--- rush03/rush03.c
+++ rush03/rush03.c
@@ -6,7 +6,7 @@
 /*   By: dhwang <dhwang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 14:00:18 by dhwang            #+#    #+#             */
-/*   Updated: 2021/10/09 15:31:26 by dhwang           ###   ########.fr       */
+/*   Updated: 2021/10/11 15:02:55 by dhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,11 @@ void	rush(int x, int y)
 	while (i <= y)
 	{
 		if (i == 1)
-			set_line(x, 'o', '-', 'o');
+			set_line(x, 'A', 'B', 'C');
 		else if (i <= (y - 1))
-			set_line(x, '|', ' ', '|');
+			set_line(x, 'B', ' ', 'B');
 		else if (i == y)
-			set_line(x, 'o', '-', 'o');
+			set_line(x, 'A', 'B', 'C');
 		ft_putchar('\n');
 		i++;
 	}
